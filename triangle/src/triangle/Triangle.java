2cc3c93604943bee5c8bbc2dd3e0681123b1ab0d
cRaji2003 <crajeshwaricrajeshwari03@gmail.com>
2023-07-14 12:03:46 +0530

    first commit

---
+++ triangle/src/triangle/Triangle.java
@@ -0,0 +1,115 @@
+
+package triangle;
+
+public class Triangle {
+
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=9;j++){
+                 if(i==5||(i==4&&j==2)||(i==4&&j==3)||(i==4&&j==4)||(i==4&&j==5)||(i==4&&j==6)||(i==4&&j==7)||(i==4&&j==8)||(i==3&&j==3)||(i==3&&j==4)||(i==3&&j==5)||(i==3&&j==6)||(i==3&&j==7)||(i==2&&j==4)||(i==2&&j==5)||(i==2&&j==6)||(i==1&&j==5)){
+                System.out.print("*"+" ");
+            } 
+            else{
+                 System.out.print("  ");
+             }
+         }
+         System.out.println();
+            }
+        System.out.println("");
+        System.out.println("");
+        for(int i=5;i>=1;i--){
+            for(int j=1;j<=9;j++){
+                 if(i==5||(i==4&&j==2)||(i==4&&j==3)||(i==4&&j==4)||(i==4&&j==5)||(i==4&&j==6)||(i==4&&j==7)||(i==4&&j==8)||(i==3&&j==3)||(i==3&&j==4)||(i==3&&j==5)||(i==3&&j==6)||(i==3&&j==7)||(i==2&&j==4)||(i==2&&j==5)||(i==2&&j==6)||(i==1&&j==5)){
+                System.out.print("*"+" ");
+            } 
+            else{
+                 System.out.print("  ");
+             }
+         }
+         System.out.println();
+            }
+        System.out.println("");
+        System.out.println("");
+        for(int i=1;i<=9;i++){
+            for(int j=1;j<=5;j++){
+             if(j==5||(j==4&&i==2)||(j==4&&i==3)||(j==4&&i==4)||(j==4&&i==5||(j==4&&i==6)||(j==4&&i==7)||(j==4&&i==8)||(j==3&&i==3)||(j==3&&i==4)||(j==3&&i==5)||(j==3&&i==6)||(j==3&&i==7)||(j==2&&i==4)||(j==2&&i==5)||(j==2&&i==6)||(j==1&&i==5))){
+                System.out.print("*"+" ");
+            } 
+            else{
+                 System.out.print("  ");
+             }
+         }
+         System.out.println();
+            }
+        System.out.println("");
+        System.out.println("");   
+        for(int i=1;i<=9;i++){
+            for(int j=1;j<=5;j++){
+             if(j==1||(j==2&&i==2)||(j==2&&i==3)||(j==2&&i==4)||(j==2&&i==5||(j==2&&i==6)||(j==2&&i==7)||(j==2&&i==8)||(j==3&&i==3)||(j==3&&i==4)||(j==3&&i==5)||(j==3&&i==6)||(j==3&&i==7)||(j==4&&i==4)||(j==4&&i==5)||(j==4&&i==6)||(j==5&&i==5))){
+                System.out.print("*"+" ");
+            } 
+            else{
+                 System.out.print("  ");
+             }
+         }
+         System.out.println();
+            }
+        System.out.println("");
+        System.out.println(""); 
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+                if(i==5||j==1||(i==4&&j==4)||(i==4&&j==3)||(i==4&&j==2)||(i==3&&j==3)||(i==3&&j==2)||(i==2&&j==2)){
+                   System.out.print("*"+" ");
+                }
+                else{
+                    
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println(" ");
+        System.out.println(" ");
+        for(int i=5;i>=1;i--){
+            for(int j=1;j<=5;j++){
+                if(i==5||j==1||(i==4&&j==4)||(i==4&&j==3)||(i==4&&j==2)||(i==3&&j==3)||(i==3&&j==2)||(i==2&&j==2)){
+                   System.out.print("*"+" ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("");
+        System.out.println("");
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+                if(j==5||i==1||(j==4&&i==4)||(j==4&&i==3)||(j==4&&i==2)||(j==3&&i==3)||(j==3&&i==2)||(j==2&&i==2)){
+                   System.out.print("*"+" ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("");
+        System.out.println("");
+        for(int i=5;i>=1;i--){
+            for(int j=1;j<=5;j++){
+                if(j==5||i==1||(j==4&&i==4)||(j==4&&i==3)||(j==4&&i==2)||(j==3&&i==3)||(j==3&&i==2)||(j==2&&i==2)){
+                   System.out.print("*"+" ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("");
+        System.out.println("");
+        }
+    }
+    
+
